0d4d343af83f311f16680117b177dba0d1a873e9
juleneuribarri <julene.uribarri@opendeusto.es>
2023-11-21 11:24:39 +0100

    maquina_estados_1

--- main.cpp
+++ main.cpp
@@ -1,46 +1,57 @@
 #include "mbed.h"
 
-enum estados {apagado, encendido} estado;
- 
+enum estados { esperando_pulsar, pulsado, esperando_soltar } estado;
+
 Timer temporizador;
- 
- 
+
+DigitalIn boton(BUTTON1); //BUTTON1 PORQUE ES DE LA PLACA
+
 DigitalOut led(LED1);
- 
-void estadoApagado()
-{
-    if(temporizador.read()>1.0f) {
-        temporizador.reset();
-        led=1;
-        estado=encendido;
-    }
-}
- 
-void estadoEncendido()
-{
-    if(temporizador.read()>0.5f) {
-        temporizador.reset();
-        led=0;
-        estado=apagado;
- 
-    }
-}
- 
-int main()
-{
-    led=0;
-    estado=apagado;
+
+void estado_esperando_pulsar() {
+  if (boton==0) {
     temporizador.reset();
     temporizador.start();
-    while(1) {
-        switch(estado) {
-            case apagado:
-                estadoApagado();
-                break;
-            case encendido:
-                estadoEncendido();
-                break;
-        }
- 
+    led = 1;
+    estado = pulsado;
+  }
+}
+
+void estado_pulsado() {
+  if (boton ==0 && temporizador.read() > 2.0f) {
+    led = 0;
+    estado = esperando_soltar;
+  }
+
+ if (boton ==1 && temporizador.read() > 2.0f) {
+    led = 0;
+    estado = esperando_pulsar;
+  }
+
+}
+
+void estado_esperando_soltar() {
+  if (boton == 1) {
+    estado = esperando_pulsar;
+  }
+}
+
+int main() {
+  led = 0;
+  estado = esperando_pulsar;
+  temporizador.reset();
+  temporizador.start();
+  while (1) {
+    switch (estado) {
+    case esperando_pulsar:
+      estado_esperando_pulsar();
+      break;
+    case pulsado:
+      estado_pulsado();
+      break;
+    case esperando_soltar:
+      estado_esperando_soltar();
+      break;
     }
+  }
 }
